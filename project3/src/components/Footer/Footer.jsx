4b2b5c8bc6654010e639942a69fd531bafe04525
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-05-13 18:54:24 -0400

    created the login and signup files

--- project3/src/components/Footer/Footer.jsx
+++ project3/src/components/Footer/Footer.jsx
@@ -1,1 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Wikipedia Searcher</span>
+    </footer>
+  );
+}
+
+export default Footer;
