c64af5485c65ae1f65be6a92ec94d3cd211009a6
Sharon Kim <skimilk19@gmail.com>
2021-05-13 19:51:56 -0500

    completed index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,33 +1,30 @@
 const User = require('./User');
+const Station = require('./Station');
+const Comment = require('./Comment');
+const Favorite = require('./Favorite');
 
-
-User.hasMany(Search, {
+User.hasMany(Favorite, {
     foreignKey: 'user_id',
     onDelete: 'CASCADE'
 });
 
-Search.belongsTo(User, {
-    foreignKey: 'user_id'
+Station.hasMany(Comment,{
+    foreignKey: 'station_id',
+});
+
+Location.hasMany(Station, {
+    foreignKey: 'location_id',
+});
+
+Favorite.belongsTo(Station, {
+    foreignKey: 'user_id',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+
 });
 
 // ***** Not sure if this is the best way to handle the associations between user/comment and search/comment??
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-});
-
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
-
-Search.hasMany(Comment, {
-    foreignKey: 'search_id',
-});
-
-Comment.belongsTo(Search, {
-    foreignKey: 'search_id',
-});
-
-
-
-module.exports = { User, Search, Comment };
+module.exports = { User, Favorite, Comment, Location};
