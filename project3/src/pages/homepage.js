05056feb5f134f51b87d1146de62a33d5f8033bb
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-05-14 17:46:52 -0400

    added loadLocation function to homepage to retrieve api JSON data

--- project3/src/pages/homepage.js
+++ project3/src/pages/homepage.js
@@ -1,1 +1,55 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import GoogleMapReact from 'google-map-react';
+import API from "../utils/API";
+
+
+function Home() {
+  // Setting our component's initial state
+  const [location, setLocation] = useState([])
+
+  // Load all books and store them with setBooks
+  useEffect(() => {
+    loadLocation()
+  }, [])
+
+  // Loads all books and sets them to books
+  function loadLocation() {
+    API.getLocation()
+      .then(res => console.log(res.data) 
+    //   setLocation(res.data)
+      )
+      .catch(err => console.log(err));
+  };
+
+//   function handleInputChange(event) {
+//     // add code to control the components here
+//     const { name, value } = event.target;
+//     const newState = {
+//       ...formObject,
+//       [name]: value
+//     }
+
+//     setFormObject(newState);
+//   }
+
+//   function handleFormSubmit() {
+//     // add code here to post a new book to the api
+//     if (formObject.title, formObject.author) {
+//       API.saveBook(formObject)
+//       .then((res) => {
+//         console.log(res.data)
+//         setFormObject(res.data);
+//       })
+//       .catch(err => console.log(err));
+//     }
+//   }
+
+    return (
+      <div>
+          <h1>Testing for API JSON data</h1>
+      </div>
+    );
+  }
+
+
+export default Home;
--- project3/src/utils/API.js
+++ project3/src/utils/API.js
@@ -6,5 +6,4 @@ export default {
     const key = "50d64c87-9aac-4af5-bc5b-695af831ea8e"
     return axios.get(`https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=10&compact=true&verbose=false&key=${key}`);
   },
-  
 };
