b466ac27486f62bad6f6b47cd8a5d0c4d303a0d5
Arash Saeedi <saeediarash84@gmail.com>
2021-05-17 13:39:02 -0400

    added onClick for the popup buttons to work and need to work on the Login/Signup css

--- client/src/components/PopUp/PopUp.jsx
+++ client/src/components/PopUp/PopUp.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
-// import "../../styles/popup";
+import "./popup.css";
+
+
+
 
 
 function Popup(props){
+
+
+
+
+
     return (props.trigger) ? (
         <div className="popup">
             <div className="popup-inner">
-                <button className="close-btn">
+                <button onClick={() => props.setTrigger(false)} className="close-btn">
                    close
                 </button>
                 {props.children}
--- client/src/pages/welcomepage.js
+++ client/src/pages/welcomepage.js
@@ -1,7 +1,15 @@
 import React from 'react';
-import Popup from "../components/PopUp/PopUp"
+import Popup from "../components/PopUp/PopUp";
+import { useState } from 'react';
+import Login from '../components/Login/Login';
+import Signup from '../components/Signup/Signup';
+
 
 function WelcomePage() {
+// this is for the popup so it can change its state and work
+    const [buttonPopup, setButtonPopup] = useState(false);
+
+
     return (
         <div className="WelcomePage">
             <header className="hero-header">
@@ -12,9 +20,11 @@ function WelcomePage() {
                     Here at EA we strive to make access to EV charging stations easy...etc.etc..
                 </p>
                 {/* maybe do an ocClick?? */}
-                <button className="get-started-btn rounded-pill">Get Started</button>
-                <Popup trigger={true}>
-                    <h3>My Popup</h3>
+                <button onClick={() => setButtonPopup(true)} className="get-started-btn rounded-pill">Get Started</button>
+                <Popup trigger={buttonPopup} setTrigger={setButtonPopup}>
+                    <Login />
+                    {/* <Signup /> */}
+                    {/* need to add CSS to this we are also working on connecting the auth key */}
                 </Popup>
            </header>
 
