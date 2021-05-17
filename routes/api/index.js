608a90bc0ef61c0a7968c46a54418ba4e1a31fd7
Sharon Kim <skimilk19@gmail.com>
2021-05-17 18:31:06 -0500

    deleted Location.js and added router to index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const commentRoutes = require("./commentRoutes");
+const  favoriteRoutes = require("./favoriteRoutes");
 
 router.use("/users", userRoutes);
 router.use("/comment", commentRoutes);
+router.use("/favorite", favoriteRoutes);
 
 module.exports = router;
