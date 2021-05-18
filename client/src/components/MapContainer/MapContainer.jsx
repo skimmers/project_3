caf619f8becca67ca2f7ac9d6f1f0676eed8fb65
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-05-17 22:05:44 -0400

    was messing around with separate npm packages.. can ignore this file for now. No code being exported to other files

--- client/src/components/MapContainer/MapContainer.jsx
+++ client/src/components/MapContainer/MapContainer.jsx
@@ -2,6 +2,131 @@ import React, { Component } from 'react';
 import  GoogleMapReact, { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import Container from "../Container/Container";
 
+// export class MapContainer extends React.Component {
+
+//   state = {
+//     showingInfoWindow: false,  // Hides or shows the InfoWindow
+//     activeMarker: {},          // Shows the active marker upon click
+//     selectedPlace: {},         // Shows the InfoWindow to the selected place upon a marker
+//     location: {},
+//     startingPoint: {}
+//   };
+  
+
+//   componentDidMount() {
+
+//     var options = {
+//       enableHighAccuracy: true,
+//       timeout: 5000,
+//       maximumAge: 0
+//     };
+    
+//     var success = (pos) => {
+//       var crd = pos.coords;
+    
+//       console.log('Your current position is:');
+//       console.log(`Latitude : ${crd.latitude}`);
+//       console.log(`Longitude: ${crd.longitude}`);
+//     }
+    
+//     var error = (err) => {
+//       console.warn(`ERROR(${err.code}): ${err.message}`);
+//     }
+//     // sets the starting point to the user's current location
+//    console.log(navigator.geolocation.getCurrentPosition(success, error, options));
+
+//     // calls loadLocation API fetch
+//     // loadLocation();
+
+//     API.getLocation()
+//       .then(res => {
+//         console.log(res.data) 
+//         this.setState({location: res.data})
+//         console.log(location);
+//       })
+//       .catch(err => console.log(err));
+//   }
+
+//   // // Loads the API data and sets state
+//   // loadLocation() {
+//   //   API.getLocation()
+//   //     .then(res => {
+//   //       console.log(res.data) 
+//   //       this.setState({location: res.data})
+//   //       console.log(location);
+//   //     })
+//   //     .catch(err => console.log(err));
+//   // };
+
+//   onMarkerClick = (props, marker, e) =>
+//   this.setState({
+//     selectedPlace: props,
+//     activeMarker: marker,
+//     showingInfoWindow: true
+//   });
+
+//   onClose = props => {
+//     if (this.state.showingInfoWindow) {
+//     this.setState({
+//       showingInfoWindow: false,
+//       activeMarker: null
+//     });
+//   }
+// };
+
+//   render() {
+//     // Height and width of map
+//     const mapStyles = {
+//       width: '100vw',
+//       height: '100vh'
+//     };
+
+//     const containerStyle = {
+//       position: 'relative',  
+//       width: '100%',
+//       height: '100%'
+//     }      
+
+//     return (
+//       <Container>
+//           <Map
+//               google={this.props.google}
+//               zoom={14}
+//               containerStyle={containerStyle}
+//               style={mapStyles}
+//               initialCenter={
+//                   {
+//                       lat: this.props.startingPoint,
+//                       lng: this.props.startingPoint
+//                   }
+//               }
+//           />
+//           {/* {this.state.location.map(loc => {
+//             return (
+//               <div>
+//                 <Marker
+//                 key={loc.ID}
+//                 onClick={this.onMarkerClick}
+//                 name={loc.AddressInfo.Title}
+//                 />
+//                 <InfoWindow
+//                 marker={this.state.activeMarker}
+//                 visible={this.state.showingInfoWindow}
+//                 onClose={this.onClose}
+//                 >
+//                 <div>
+//                     <h4>{this.state.selectedPlace.name}</h4>
+//                 </div>
+//                 </InfoWindow>
+//               </div>
+//             )
+//           })} */}
+
+//       </Container>        
+//       );
+//   }
+// }
+
 export class MapContainer extends Component {
 
     state = {
@@ -9,6 +134,7 @@ export class MapContainer extends Component {
         activeMarker: {},          // Shows the active marker upon click
         selectedPlace: {}          // Shows the InfoWindow to the selected place upon a marker
       };
+      
 
     onMarkerClick = (props, marker, e) =>
     this.setState({
@@ -27,40 +153,52 @@ export class MapContainer extends Component {
   };
 
     render() {
-  
       // Height and width of map
       const mapStyles = {
+        width: '100vw',
+        height: '100vh'
+      };
+
+      const containerStyle = {
+        position: 'relative',  
         width: '100%',
         height: '100%'
-      };
+      }      
   
       return (
         <Container>
             <Map
                 google={this.props.google}
                 zoom={14}
+                containerStyle={containerStyle}
                 style={mapStyles}
                 initialCenter={
                     {
-                        lat: -1.2884,
-                        lng: 36.8233
+                        lat: this.props.startingPoint,
+                        lng: this.props.startingPoint
                     }
                 }
             />
-
-            <Marker
-            onClick={this.onMarkerClick}
-            name={'Kenyatta International Convention Centre'}
-            />
-            <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}
-            onClose={this.onClose}
-            >
-            <div>
-                <h4>{this.state.selectedPlace.name}</h4>
-            </div>
-            </InfoWindow>
+            {/* {this.state.location.map(loc => {
+              return (
+                <div>
+                  <Marker
+                  key={loc.ID}
+                  onClick={this.onMarkerClick}
+                  name={loc.AddressInfo.Title}
+                  />
+                  <InfoWindow
+                  marker={this.state.activeMarker}
+                  visible={this.state.showingInfoWindow}
+                  onClose={this.onClose}
+                  >
+                  <div>
+                      <h4>{this.state.selectedPlace.name}</h4>
+                  </div>
+                  </InfoWindow>
+                </div>
+              )
+            })} */}
 
         </Container>        
         );
