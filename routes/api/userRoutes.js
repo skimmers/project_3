d02d1735a316ef030887da661d6b8750032ff146
Sharon Kim <skimilk19@gmail.com>
2021-05-19 18:22:14 -0500

    tested routes and seeded data

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,7 +6,9 @@ const withAuth = require('../../utils/auth');
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
-
+    
+    console.log(userData);
+    
     if (!userData) {
       res
         .status(400)
