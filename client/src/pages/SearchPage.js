1e591711794fd52a10068aed47eded53ffcdf9f6
Sharon Kim <skimilk19@gmail.com>
2021-05-20 15:33:40 -0500

    created a search page with includes a searchbar

--- client/src/pages/SearchPage.js
+++ client/src/pages/SearchPage.js
@@ -1,54 +1,38 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import ShareIcon from '@material-ui/icons/Share';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import React, {useState, useEffect} from 'react';
 import SearchBar from '../components/SearchBar/SearchBar';
-import MapList from '../components/Map/Map';
+import SearchResult from '../components/SearchResult';
 
 const SearchPage = (props) => {
   const [input, setInput] = useState('');
-  const [mapDefault, setMapDefault] = useState();
-  const [mapList, setMapList] = useState();
+  const [searchResultDefault, setSearchResultDefault] = useState();
+  const [searchResult, setSearchResult] = useState();
 
   const fetchData = async () => {
     return await fetch('https://restcountries.eu/rest/v2/all')
       .then(response => response.json())
       .then(data => {
-         setMapList(data) 
-         setMapListDefault(data)
+         setSearchResult(data) 
+         setSearchResultDefault(data)
        });}
 
   const updateInput = async (input) => {
-     const filtered = countryListDefault.filter(country => {
-      return country.name.toLowerCase().includes(input.toLowerCase())
+     const filtered = searchResultDefault.filter(search => {
+      return search.name.toLowerCase().includes(input.toLowerCase())
      })
      setInput(input);
-     setMapList(filtered);
+     setSearchResult(filtered);
   }
 
   useEffect( () => {fetchData()},[]);
 	
   return (
     <>
-      <h1>Country List</h1>
+      <h1>Search Results</h1>
       <SearchBar 
        input={input} 
        onChange={updateInput}
       />
-      <MapList mapList={mapList}/>
+      <SearchResult searchResult={searchResult}/>
     </>
    );
 }
