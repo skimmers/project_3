b0b95da270b25802e0f70a75426cb40265007776
Sharon Kim <skimilk19@gmail.com>
2021-05-19 23:46:08 -0500

    added searchbar.jsx

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 
-function SearchBar(props){
+//function SearchBar(props){
+    //return (
+       //<input type="search"
+       //className="search-bar"
+       //placeholder={props.placeholder}
+       //onChange={props.handleChange} 
+       ///>
+    //)
+//}
+
+const SearchBar = ({keyword,setKeyword}) => {
+    const BarStyling = {width:"20rem",background:"#F2F1F9", border:"none", padding:"0.5rem"};
     return (
-       <input type="search"
-       className="search-bar"
-       placeholder={props.placeholder}
-       onChange={props.handleChange} 
-       />
-    )
-}
+      <input 
+       style={BarStyling}
+       key="random1"
+       value={keyword}
+       placeholder={"search place"}
+       onChange={(e) => setKeyword(e.target.value)}
+      />
+    );
+  }
 
 export default SearchBar;
