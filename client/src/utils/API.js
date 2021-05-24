d7afa59564a5ce0c670e636fb68f83f434fbf3cc
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-05-23 20:02:19 -0400

    readded dotenv npm package, reset .gitignore, and added API keys to .env files

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,21 +1,25 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
+require('dotenv').config();
 
 export default {
-  // Gets all EV Charge stations
+  // API call to retrieve all EV Charge stations for a given location
   getLocation: function(lat, lng) { 
-    const key = "50d64c87-9aac-4af5-bc5b-695af831ea8e"
+    const key = process.env.OPEN_CHARGE_MAP_KEY;
     return axios.get(`https://api.openchargemap.io/v3/poi/?output=json&countrycode=US&maxresults=25&compact=true&verbose=false&key=${key}&latitude=${lat}&longitude=${lng}&distance=25`);
   },
 
+  // Function that calls the authcheck route in order to verify a user is signed in
   checkLoginStatus: function () {
     return axios.get('/api/users/authcheck');
   },
 
+  // Retrieves all favorited stations for a given user
   getFavorites: function () {
     return axios.get('/api/favorite');
   },
 
+  // Deletes a favorited station for a given user
   deleteFavorite: function (id) {
     return axios.delete(`/api/favorite/${id}`)
   }
