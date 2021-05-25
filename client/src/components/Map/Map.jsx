edf11d4736c35a55e22e2d9205c872509b037eda
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-05-25 00:28:29 -0400

    map bug now fixed

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -6,7 +6,7 @@ const MapWithMarkers = withScriptjs(withGoogleMap(props =>
 
     <GoogleMap
       defaultZoom={13}
-      defaultCenter={{ lat: props.initPosition.lat, lng: props.initPosition.lng }}
+      center={{ lat: props.initPosition.lat, lng: props.initPosition.lng }}
     >
       {props.isMarkerShown && props.location.map((loc, index) => {
         const [evStation, setEvStation] = useState(null);
