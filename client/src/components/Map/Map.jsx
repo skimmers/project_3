e2d39eb561606343172e251bf6c455c1ff6cb3ff
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-05-25 00:23:17 -0400

    fixed bug that requires page refresh for map to render

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { GoogleMap, Marker, withScriptjs, withGoogleMap, InfoWindow } from "react-google-maps";
 import PopupInfo from "../PopupInfo/PopupInfo";
 
