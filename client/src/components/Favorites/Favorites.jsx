5b54c0d2c6e9db2369237eb080bf2ed9e41548ec
Tyler Gilbert <tyler.gilbert913@gmail.com>
2021-05-24 22:32:34 -0400

    tryin to fix mysql issues

--- client/src/components/Favorites/Favorites.jsx
+++ client/src/components/Favorites/Favorites.jsx
@@ -15,7 +15,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
-import DeleteIcon from '@material-ui/icons/Delete';
+import PopupInfo from "../PopupInfo.js";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,10 +40,10 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function FavoritesInfo(props) {
+export default function StationInfo() {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
-  
+
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
@@ -63,17 +63,18 @@ export default function FavoritesInfo(props) {
             <MoreVertIcon />
           </IconButton>
         }
-        title={props.favorite.title}
-        subheader={props.favorite.city}
-      >
-      </CardHeader>
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
       <CardMedia
         className={classes.media}
-        // image="/static/images/cards/paella.jpg"
-        // title="Paella dish"
+        image="/static/images/cards/paella.jpg"
+        title={}
       />
       <CardContent>
         <Typography variant="body2" color="textSecondary" component="p">
+          This impressive paella is a perfect party dish and a fun meal to cook together with your
+          guests. Add 1 cup of frozen peas along with the mussels, if you like.
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
@@ -93,32 +94,31 @@ export default function FavoritesInfo(props) {
         >
           <ExpandMoreIcon />
         </IconButton>
-        <IconButton aria-label="delete">
-          <DeleteIcon 
-            onClick={props.handleDelete}
-          />
-      </IconButton>
       </CardActions>
       <Collapse in={expanded} timeout="auto" unmountOnExit>
         <CardContent>
-          <Typography paragraph>Information:</Typography>
+          <Typography paragraph>Method:</Typography>
           <Typography paragraph>
-            Address: {props.favorite.address}
+            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+            minutes.
           </Typography>
           <Typography paragraph>
-            Access: {props.favorite.access}
+            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
           </Typography>
           <Typography paragraph>
-            Power KW: {props.favorite.power}
+            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+            again without stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don’t open.)
           </Typography>
-          <Typography paragraph>
-            Voltage: {props.favorite.voltage}Mw
-          </Typography>
-          <Typography paragraph>
-            Connection Type: {props.favorite.connectionType}
-          </Typography>
-          <Typography paragraph>
-            Station Site: {props.favorite.stationSite}
+          <Typography>
+            Set aside off of the heat to let rest for 10 minutes, and then serve.
           </Typography>
         </CardContent>
       </Collapse>
