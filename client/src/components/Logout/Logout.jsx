89aa23ec0f65a20c9cf7766434f9299517062f8a
Jonathan Stoll <73194238+jonathanstoll0603@users.noreply.github.com>
2021-05-26 16:29:30 -0400

    removed unused code

--- client/src/components/Logout/Logout.jsx
+++ client/src/components/Logout/Logout.jsx
@@ -3,10 +3,6 @@ import React from 'react';
 
 function Logout(props) {
 
-    console.log(props);
-    // const handleLogout = () => {
-    // };
-
     return (
         <div>
             <button className="sign-up-btn rounded-pill" onClick={props.logoutHandler}>Logout</button>
